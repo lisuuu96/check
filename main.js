bf1d672372c68d84b53fec500b1fa05b4a69f49d
lisuuu96 <adrian.lis1996@gmail.com>
2023-01-05 08:35:36 +0100

    check

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 let num1 = n
 function check(num1){
-    if (n>100 & n<200)
+    if (n>100 & n<200){
     console.log('Liczba znajduje się w przedziale');
-    else(n<100 &n>200);
+}
+    else{
     console.log(`Liczba nie znajduje się w przedziale`);
+    }
 }
     
 
